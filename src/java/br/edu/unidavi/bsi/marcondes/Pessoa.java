41b8c100dbaec9b82262e5ae6137a36a542a05c4
marcondes <marcondesmacaneiro@gmail.com>
2013-05-28 19:07:18 -0300

    Package Marcondes

---
+++ src/java/br/edu/unidavi/bsi/marcondes/Pessoa.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.unidavi.bsi.marcondes;
+
+/**
+ *
+ * @author marcondes
+ */
+public class Pessoa {
+    
+}
