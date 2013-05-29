4708565df55666e4501791440505e42ca21918aa
marcondes <marcondesmacaneiro@gmail.com>
2013-05-28 21:58:36 -0300

    Configurações gerais do projeto.

---
+++ src/java/br/edu/unidavi/bsi/marcondes/locator/ServiceLocator.java
@@ -0,0 +1,38 @@
+package br.edu.unidavi.bsi.marcondes.locator;
+
+import java.sql.*;
+
+import br.edu.unidavi.bsi.marcondes.exception.UnidaviBsiException;
+
+
+public class ServiceLocator {
+    
+    private String databaseURL = "jdbc:mysql://localhost:3306/aw";
+    
+    private String databasePassword = "root";
+    
+    private String databaseUser = "root";
+    
+    private boolean useDatasource = false;
+    
+    private static ServiceLocator instance = new ServiceLocator();
+    
+    private ServiceLocator() {
+    }
+    
+    public static ServiceLocator getInstance() {
+        return instance;
+    }
+    
+    public Connection getConnection() throws UnidaviBsiException {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(databaseURL, databaseUser,databasePassword);
+        } catch (Exception e) {
+            System.out.println("Erro fatal ao obter conexao com DriverManager: "+ e.getMessage());
+            throw new UnidaviBsiException("Erro ao obter conexao via DriverManager: "+ e.getMessage(),e);
+        }
+        return conn;
+    }
+}
--- web/marcondes/index.jsp
+++ web/marcondes/index.jsp
@@ -12,6 +12,6 @@
         <title>JSP Page</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+       <a href='<%=request.getContextPath()%>/marcondes/cadastroCliente.jsp'> Clique aqui para ir para o Cadastro do Cliente </a>
     </body>
 </html>
