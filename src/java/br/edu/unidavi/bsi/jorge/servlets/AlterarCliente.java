7be1f7c77c2b2d7db186e9d40f5644c7837dc81e
Jeferson Duwe <Jeferson Duwe@192.168.1.14>
2013-06-18 21:18:13 -0300

    Parte do Jorge, o PC Dele Bugou

---
+++ src/java/br/edu/unidavi/bsi/jorge/servlets/AlterarCliente.java
@@ -0,0 +1,109 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.unidavi.bsi.jorge.servlets;
+
+import br.edu.unidavi.bsi.jorge.exeption.UnidaviBsiException;
+import br.edu.unidavi.bsi.jorge.jdbc.ClienteDAO;
+import br.edu.unidavi.bsi.jorge.model.Cliente;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Jorge
+ */
+@WebServlet(name = "AlterarCliente", urlPatterns = {"/AlterarCliente"})
+public class AlterarCliente extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    
+    /*parametro pega os dados digitados e armazenadas nas variaveis, o request criar um parametro "Cliente"
+     já citando esses dados, e depois da um comando para salvar os dados no "Cliente" */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        String nome = request.getParameter("nome");
+        String telefone = request.getParameter("telefone");
+        String email = request.getParameter("email");
+        String rua = request.getParameter("rua");
+        String numero = request.getParameter("numero");
+        String bairro = request.getParameter("bairro");
+        String cep = request.getParameter("cep");
+        String cidade = request.getParameter("cidade");
+        String estado = request.getParameter("estado");
+        Cliente cliente = new Cliente(id, nome, email, null, telefone, true, rua, numero, bairro, cep, cidade, estado);
+        String mensagem = "Cliente Alterado com sucesso.";
+        try{
+            ClienteDAO.getInstance().save(cliente);
+        } catch(UnidaviBsiException e){
+            e.getCause().printStackTrace();
+            mensagem = "Não foi possível alterar o cliente selecionado";
+        }
+        
+        request.setAttribute("mensagem", mensagem);
+        /* Ele manda o parametro para buscarclientes para o sistema realizar novamente uma busca e atualizar
+         a lista e depois o busca clientes mostra novamente a lista de usuarios*/
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/BuscaClientes");
+        dispatcher.forward(request, response);
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
